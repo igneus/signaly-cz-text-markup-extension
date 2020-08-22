92aa3a0537a87880a6e5c280b4c3097968bb0967
Jakub Pavlik <severus@post.cz>
2020-08-22 23:02:42 +0200

    get rid of repeated assignment

--- extension.js
+++ extension.js
@@ -4,26 +4,28 @@ const renderCommentsMarkup = () => document
     .querySelectorAll('.comments .one__comment p')
     .forEach(node => {
         console.log(node);
-        node.innerHTML = node.innerHTML.replace(
-                /&lt;([A-Fa-f0-9]{6})&gt;/g,
-            (match, group1) => {
-                const colour = group1.toLowerCase();
-                let title = null;
-                if ('ff00ff' == colour) {
-                    title = 'tohle je vážně';
+        node.innerHTML =
+            node.innerHTML
+            .replace(
+                    /&lt;([A-Fa-f0-9]{6})&gt;/g,
+                (match, group1) => {
+                    const colour = group1.toLowerCase();
+                    let title = null;
+                    if ('ff00ff' == colour) {
+                        title = 'tohle je vážně';
+                    }
+                    if ('800080' == colour) {
+                        title = 'tohle je polovážně/položertem';
+                    }
+                    return '<span style="color: #' + group1 + ';"' +
+                        (title ? (' title="' + title + '"') : '') +
+                        '>';
                 }
-                if ('800080' == colour) {
-                    title = 'tohle je polovážně/položertem';
-                }
-                return '<span style="color: #' + group1 + ';"' +
-                    (title ? (' title="' + title + '"') : '') +
-                    '>';
-            }
-        );
-        node.innerHTML = node.innerHTML.replace(
-                /&lt;\/([A-Fa-f0-9]{6})&gt;/g,
-            '</span>'
-        );
+            )
+            .replace(
+                    /&lt;\/([A-Fa-f0-9]{6})&gt;/g,
+                '</span>'
+            );
     });
 
 renderCommentsMarkup();
